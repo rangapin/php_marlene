5ec9e787301ddfd4228d00b4732519dca0f8d951
rangapin <rangapin@gmx.com>
2022-11-20 21:45:48 -0600

    day-five-styling the app

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Marlene</title>
+	<meta name="description" content="Search the web for URLs.">
+	<meta name="keywords" content="Search engine, marlene, websites">
+	<meta name="author" content="Richard Angapin">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+	<div class="wrapper indexPage">
+
+		<div class="mainSection">
+
+			<div class="logoContainer">
+
+				<img src="assets/images/marleneLogo.png" title="Logo of the site" alt="Site logo">
+
+			</div>
+
+
+			<div class="searchContainer">
+
+				<form action="search.php" method="GET">
+
+					<input class="searchBox" type="text" name="term" autocomplete="off">
+
+					<input class="searchButton" type="submit" value="Search">
+
+				</form>
+
+			</div>
+
+
+		</div>
+
+
+	</div>
+
+</body>
+</html>
