b72d32fb6641a1447711dd5d40e96d94df11af47
rangapin <rangapin@gmx.com>
2022-11-21 23:25:47 -0600

    project completed

--- search.php
+++ search.php
@@ -30,9 +30,13 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.css" />
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
 	<title>Marlene</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+
 </head>
 
 <body>
